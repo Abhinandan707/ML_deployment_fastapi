b2f0e7517dd6bfd7ddce254bc388d3c603f82b0c
Abhinandan <abhinandan.kchand.2016@gmail.com>
2024-07-31 12:33:56 +0200

    added_api_POST

--- main.py
+++ main.py
@@ -1,1 +1,84 @@
-# Put the code for your API here.
+from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel, Field
+import pickle
+import os
+from ml.data import process_data
+from ml.model import inference
+import pandas as pd
+import numpy as np
+
+if os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+        
+# Initialize API object
+app = FastAPI()
+
+# Load model
+file_dir = os.path.dirname(__file__)
+model_path = os.path.join(file_dir, './model/rf_model.pkl')
+encoder_path = os.path.join(file_dir, './model/encoder.pkl')
+lb_path = os.path.join(file_dir, './model/lb.pkl')
+
+model = pickle.load(open(model_path, 'rb'))
+encoder = pickle.load(open(encoder_path, 'rb'))
+lb = pickle.load(open(lb_path, 'rb'))
+
+
+class InputData(BaseModel):
+    # Using the first row of census.csv as sample
+    age: int = Field(None, example=25)
+    workclass: str = Field(None, example='State-gov')
+    fnlgt: int = Field(None, example=54545)
+    education: str = Field(None, example='Masters')
+    education_num: int = Field(None, example=13)
+    marital_status: str = Field(None, example='Never-married')
+    occupation: str = Field(None, example='Adm-clerical')
+    relationship: str = Field(None, example='Not-in-family')
+    race: str = Field(None, example='White')
+    sex: str = Field(None, example='Female')
+    capital_gain: int = Field(None, example=420)
+    capital_loss: int = Field(None, example=200)
+    hours_per_week: int = Field(None, example=45)
+    native_country: str = Field(None, example='Bharat')
+
+
+@app.get('/')
+async def welcome():
+    return "Welcome boss!"
+
+
+@app.post('/predict')
+async def predict(data: InputData):
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    sample = {key.replace('_', '-'): [value] for key, value in data.__dict__.items()}
+    input_data = pd.DataFrame.from_dict(sample)
+    X, _, _, _ = process_data(
+        input_data,
+        categorical_features=cat_features,
+        label=None,
+        training=False,
+        encoder=encoder,
+        lb=lb
+    )
+    output = inference(model=model, X=X)[0]
+    str_out = '<=50K' if output == 0 else '>50K'
+    return {"pred": str_out}
+
+
+if __name__ == '__main__':
+    config = uvicorn.config("main:app", host="127.0.0.1",
+                            reload=True, port=8080, log_level="info")
+    server = uvicorn.Server(config)
+    server.run()
